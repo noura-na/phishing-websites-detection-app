5fdb2b0ac73e76cc05a84d6a34e1151b582eeb81
Noura Abdelhafez <44889573+noura-na@users.noreply.github.com>
2022-04-21 18:19:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import pandas as pd
 import urllib.request
 
 url = 'https://github.com/noura-na/phishing-websites-detection-app/releases/tag/model/download/model.sav'
-url2 = 'https://github.com/noura-na/phishing-websites-detection-app/releases/tag/latest/download/model.sav.zip'
+url2 = 'https://github.com/noura-na/phishing-websites-detection-app/releases/tag/model/latest/download/model.sav.zip'
 #/data/releases/download/VacbagModelWeights/unet_vacbag_512_dsc_epoch_120.hdf5'
 filename = url.split('/')[-1]
 
