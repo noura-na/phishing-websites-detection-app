7d528ff1adb140c71a567f21cf8ebe2e32078230
Noura Abdelhafez <44889573+noura-na@users.noreply.github.com>
2022-04-21 19:00:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ from io import BytesIO
 
 @st.cache
 def load_model(url):
-	#object = pd.read_pickle(url2)
+#	object = pd.read_pickle(url2)
 #  	with open("https://github.com/noura-na/phishing-websites-detection-app/releases/download/tag/model/model.sav", 'rb') as res:
 # 	model = pickle.load(filename)
 # 	from urllib.request import urlopen
@@ -558,8 +558,8 @@ def load_model(url):
 # 	#with gzip.open('test.pklz', 'rb') as ifp: #urlopen
 # 	loaded_pickle_object = cp.load(gzip.open("https://drive.google.com/uc?export=download&id=1cKb_kPBnu8meKEmJy5ooux6sg5oQZwaH")) 
 # 	return loaded_pickle_object
-	modelLink = url
-   	model = requests.get(modelLink).content
+#	modelLink = url
+   	model = requests.get(url).content
     	return model
 
 
