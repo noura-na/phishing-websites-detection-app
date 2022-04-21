933464b597f7efb539575f3ff7575a4a370559c6
Noura Abdelhafez <44889573+noura-na@users.noreply.github.com>
2022-04-21 18:02:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ df.drop(df.index[-1], inplace = True)
 @st.cache
 def load_model():
 # 	with open("https://github.com/noura-na/phishing-websites-detection-app/releases/download/tag/model/model.sav", 'rb') as res:
-	model = pickle.load(filename, 'rb')
+	model = pickle.load(filename)
 	return model
 
 model = load_model()
